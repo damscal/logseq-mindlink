c189e8c0221ec889041e58ece6e50a1934528730
damscal <55465033+damscal@users.noreply.github.com>
2024-09-08 20:25:38 +0000

    up

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,57 @@
-// Import dotenv and configure it
-import * as dotenv from 'dotenv';
-dotenv.config();
-// Access environment variables
-const gemini_api_key = process.env.GOOGLE_API_KEY;
+import { logseq } from "@logseq/libs";
 
-// importing classes
-import {
-    SimpleDirectoryReader,
-    VectorStoreIndex,
-    MarkdownReader,
-    Gemini,
-    GEMINI_MODEL,
-    GeminiEmbedding,
-    Settings,
-} from "llamaindex";
+// Register plugin
+logseq.ready(() => {
+  console.log("Logseq Mindlink plugin loaded!");
 
-// define a markdown parser with removeHyperlinks=false and removeImages= false
-const parser = new MarkdownReader(false,false);
+    // Import dotenv and configure it
+    import * as dotenv from 'dotenv';
+    dotenv.config();
+    // Access environment variables
+    const gemini_api_key = process.env.GOOGLE_API_KEY;
 
-// load documents
-const reader = new SimpleDirectoryReader();
-const documents1 = await reader.loadData({
-    directoryPath: "/logseq/journals",
-    defaultReader: parser,
-});
-const documents2 = await reader.loadData({
-    directoryPath: "/logseq/pages",
-    defaultReader: parser,
-});
-const documents = documents1.concat(documents2);
+    // importing classes
+    import {
+        SimpleDirectoryReader,
+        VectorStoreIndex,
+        MarkdownReader,
+        Gemini,
+        GEMINI_MODEL,
+        GeminiEmbedding,
+        Settings,
+    } from "llamaindex";
+
+    // define a markdown parser with removeHyperlinks=false and removeImages= false
+    const parser = new MarkdownReader(false,false);
+
+    // load documents
+    const reader = new SimpleDirectoryReader();
+    const documents1 = await reader.loadData({
+        directoryPath: "/logseq/journals",
+        defaultReader: parser,
+    });
+    const documents2 = await reader.loadData({
+        directoryPath: "/logseq/pages",
+        defaultReader: parser,
+    });
+    const documents = documents1.concat(documents2);
 
 
-Settings.embedModel = new GeminiEmbedding();
-Settings.llm = new Gemini({
-    model: GEMINI_MODEL.GEMINI_PRO,
-  });
+    Settings.embedModel = new GeminiEmbedding();
+    Settings.llm = new Gemini({
+        model: GEMINI_MODEL.GEMINI_PRO,
+    });
 
-const index = await VectorStoreIndex.fromDocuments(documents);
+    const index = await VectorStoreIndex.fromDocuments(documents);
 
-const queryEngine = index.asQueryEngine();
+    const queryEngine = index.asQueryEngine();
 
-const query = "cosa ho discusso con il geometra Lovecchio?";
+    const query = "cosa ho discusso con il geometra Lovecchio?";
 
-const results = await queryEngine.query({
-  query,
-});
+    const results = await queryEngine.query({
+    query,
+    });
 
-console.log(results);
+    console.log(results);
+
+}).catch(console.error);
